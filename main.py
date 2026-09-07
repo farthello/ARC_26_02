6116acd9863d023b8faebf4b9a361eac2e34c2f6
fart <cillu.shin@gmail.com>
2026-09-07 16:21:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
 # Hej med dig 
 # :3
 #ser om bæstet virker...
+from pathlib import Path
+import ifcopenshell
+
+modelname = "B308X.ifc"
+
+try:
+    dir_path = Path(__file__).parent
+    model_url = Path.joinpath(dir_path, 'model', modelname).with_suffix('.ifc')
+    model = ifcopenshell.open(model_url)
+except OSError:
+    try:
+        import bpy
+        model_url = Path.joinpath(Path(bpy.context.space_data.text.filepath).parent, 'model', modelname).with_suffix('.ifc')
+        model = ifcopenshell.open(model_url)
+    except OSError:
+        print(f"ERROR: please check your model folder : {model_url} does not exist")
+
+# Your script goes here
+
+# Test if everything works:
+spaces = model.by_type("IfcSpace")
+for space in spaces:
+    print(space.LongName)
